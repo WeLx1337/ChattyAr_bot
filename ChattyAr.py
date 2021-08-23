95c22dfcfd263a34ac81fb0b781d051dc16fd264
WeLx <55147449+WeLx1337@users.noreply.github.com>
2021-08-23 05:59:58 +0200

    Update ChattyAr.py

--- ChattyAr.py
+++ ChattyAr.py
@@ -1,18 +1,1 @@
-import os
-import requests
-import telegram.ext
-TOKEN = '1917773290:AAFK2yGFDJCUwLcdmiIdn8VfVGcOtPLwQ6A'
-def start(update, context):
-  update.message.reply_text("أخبرني عن ماذا تريد أن تتحدث؟")
-def chab(update, context):
-  A = requests.get(f'https://w-aqol.com/search?s={update.message.text}').text
-  A = A.split("<td>")[2]
-  RD = A.split("</td>")[0]
-  print(RD)
-  update.message.reply_text(RD)
-updater = telegram.ext.Updater(TOKEN, use_context=True)
-disp = updater.dispatcher
-disp.add_handler(telegram.ext.CommandHandler('start', start))
-disp.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, chab))
-updater.start_polling()
-updater.idle()
+
